de6cbf271a6a9a096a9b458b8c144ba417767c9e
Abhinav Tyagi <abhinavtyagi9068@gmail.com>
2026-08-04 02:11:58 +0530

    Time: 3 ms (30.62%) | Memory: 21.7 MB (48.81%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,36 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<vector<int>>store;
-        for (int i=0;i<intervals.size();i++){
-            int s=intervals[i][0];
-            int l=intervals[i][1];
-            vector<int>temp;
-            temp.push_back(s);
-            temp.push_back(l);
-            store.push_back(temp);
-        }
-      store.push_back(newInterval);
-     sort(store.begin(),store.end());
-     int minf=INT_MAX;
+        intervals.push_back(newInterval);
+        sort(intervals.begin(),intervals.end());
+        int minf=INT_MAX;
         int maxl=INT_MIN;
         vector<vector<int>>ans;
-        for (int i=0;i<store.size();i++){
+        for (int i=0;i<intervals.size();i++){
             if (minf==INT_MAX&&maxl==INT_MIN){
-                minf=store[i][0];
-                maxl=store[i][1];
+                minf=intervals[i][0];
+                maxl=intervals[i][1];
             }
-            else if (store[i][0]>maxl){
+            else if (intervals[i][0]>maxl){
                 vector<int>temp;
                 temp.push_back(minf);
                 temp.push_back(maxl);
                 ans.push_back(temp);
-                minf=store[i][0];
-                maxl=store[i][1];
+                minf=intervals[i][0];
+                maxl=intervals[i][1];
             }
             else{
-                 minf=min(store[i][0],minf);
-                maxl=max(store[i][1],maxl);
+                 minf=min(intervals[i][0],minf);
+                maxl=max(intervals[i][1],maxl);
             }
         }
         if (minf!=INT_MIN&&maxl!=INT_MIN){
