0035571af92b782c8fcf7f3eb935a1bc5babb065
Abhinav Tyagi <abhinavtyagi9068@gmail.com>
2026-08-05 15:13:17 +0530

    Time: 6 ms (7.94%) | Memory: 26 MB (5.12%) - LeetSync

---
+++ 135-candy/candy.cpp
@@ -0,0 +1,45 @@
+class Solution {
+    void dfs(vector<int>&ratings, vector<int>& vis,vector<int>&store,int&cnt,int node){
+         
+          
+           if (!vis[node]){
+            if (ratings[node+1]<ratings[node]){
+                dfs(ratings,vis,store,cnt,node+1);
+            }
+            
+           
+           }
+           vis[node]=1;
+            store[node]=cnt;
+            cnt=cnt+1;
+           return ;
+    }
+public:
+    int candy(vector<int>& ratings) {
+        vector<int>small;
+        vector<int>vis(ratings.size(),0);
+        vis[ratings.size()-1]=1;
+        vector<int>store(ratings.size(),0);
+        store[ratings.size()-1]=0;
+        for (int i=0;i<ratings.size();i++){
+             if (!vis[i]){
+                int cnt=0;
+                dfs(ratings,vis,store,cnt,i);
+             }
+        }
+        int candy=0;
+        vector<int>s;
+        for (int i=0;i<store.size();i++){
+            int cnt=1;
+            if (i>0){
+                if (ratings[i]>ratings[i-1]){
+                    cnt=s[i-1]+1;
+                }
+            }
+          
+            candy+=max(cnt,store[i]+1);
+            s.push_back(max(cnt,store[i]+1));
+        }
+        return candy;
+    }
+};
