3c6055395c62f519fef522bc5620e82780c52e01
Abhinav Tyagi <abhinavtyagi9068@gmail.com>
2026-08-08 15:14:22 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (90.23%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -4,17 +4,17 @@ public:
         if (nums.size()==1){
             return nums[0];
         }
-       
         int maxamt=INT_MIN;
-        maxamt=max(nums[0],nums[1]);
-        vector<int> dp(nums.size(),INT_MIN);
-        dp[0]=nums[0];
-        dp[1]=nums[1];
-        for (int i=2;i<nums.size();i++){
-            for (int j=i-2;j>=0;j--){
-                dp[i]=max(dp[i],nums[i]+dp[j]);
-                maxamt=max(maxamt,dp[i]);
-            }
+        int max1=nums[0];
+        int max2=nums[1];
+        maxamt=max(max1,max2);
+        int n=nums.size();
+        for (int i=2;i<n;i++){
+            int currmax=max1+nums[i];
+            max1=max(max1,max2);
+            max2=max(max2,currmax);
+            maxamt=max(maxamt,currmax);
+
         }
         return maxamt;
     }
