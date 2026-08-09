fc6f09da3d88507fbcf141bb0dd61f47822e6c66
Abhinav Tyagi <abhinavtyagi9068@gmail.com>
2026-08-09 14:17:41 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (31.29%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,32 +1,22 @@
 class Solution {
-    int steps=0;
 public:
-    int findpaths(int m,int n ,int r,int c,vector<vector<int>>&dp){
-        int step=0;
-        
-        if (dp[r][c]!=0){
-            steps+=dp[r][c];
-            return dp[r][c];
-        }
-        int dr=1,dc=1;
-        if (r+dr<m){
-           int val=findpaths(m,n,r+dr,c,dp);
-            
-             step+=val;
-        }
-        if (c+dc<n){
-            int val=findpaths(m,n,r,c+dc,dp);
-
-                step+=val;
-        }
-        dp[r][c]=step;
-      return step;
-    }
     int uniquePaths(int m, int n) {
         vector<vector<int>>dp(m,vector<int>(n,0));
-        dp[m-1][n-1]=1;
-        int i=0,j=0;
-        int ans=findpaths(m,n,i,j,dp);
-        return steps;
+        for (int i=0;i<m;i++){
+            for (int j=0;j<n;j++){
+                if (i==0){
+                    dp[i][j]=1;
+                }
+                else{
+                  if (i>0){
+                    dp[i][j]+=dp[i-1][j];
+                  }
+                  if (j>0){
+                    dp[i][j]+=dp[i][j-1];
+                  }
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
