d9258d30b1a46083c311175ea7c1e38ca6241a50
Abhinav Tyagi <abhinavtyagi9068@gmail.com>
2026-08-10 01:47:56 +0530

    Time: 4 ms (15.58%) | Memory: 16.3 MB (42.99%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,36 +1,32 @@
 class Solution {
-    int maxsum=INT_MIN;
-    int minsum(vector<vector<int>>& mat,int r,int c,vector<vector<int>>& dp){
-        if(dp[r][c]!=-1){
-            return dp[r][c];
-        }
-
-        int dr=-1,dc=-1;
-        int up=INT_MAX,left=INT_MAX;
-        if (r+dr>=0){
-            if (up==INT_MAX){
-                up=0;
-            }
-             up+=minsum(mat,r+dr,c,dp);
-        }
-        if (c+dc>=0){
-             if (left==INT_MAX){
-                left=0;
-            }
-         left+=minsum(mat,r,c+dc,dp);
-        }
-        dp[r][c]=mat[r][c]+min(up,left);
-        return dp[r][c];
-    }
 public:
     int minPathSum(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
+        vector<vector<int>> dp(n,vector<int>(m,0));
         dp[0][0]=grid[0][0];
-        int ans=minsum(grid,n-1,m-1,dp);
-        return ans;
+        for (int i=1;i<m;i++){
+            dp[0][i]=grid[0][i]+dp[0][i-1];
+             }
+             int dr=-1,dc=-1;
+        for (int i=1;i<n;i++){
+            for (int j=0;j<m;j++){
+                int up=INT_MAX,left=INT_MAX;
+                if (i+dr>=0){
+                    if (up==INT_MAX){
+                        up=0;
+                    }
+                    up+=dp[i+dr][j];
+                }
+                if (j+dc>=0){
+                    if (left==INT_MAX){
+                        left=0;
+                    }
+                    left+=dp[i][j+dc];
+                }
+                dp[i][j]=grid[i][j]+min(up,left);
+            }
+        }
+        return dp[n-1][m-1];
     }
 };
-
-// you can move in the four direction
