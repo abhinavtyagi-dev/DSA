6b77ab36771e4fddda063a81757552be5888e91f
Abhinav Tyagi <abhinavtyagi9068@gmail.com>
2026-08-09 22:41:05 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (85.90%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -6,23 +6,30 @@ public:
         if (obstacleGrid[0][0]==1||obstacleGrid[n-1][m-1]==1){
             return 0;
         }
-        vector<vector<int>> dp(n,vector<int>(m,0));
-        dp[0][0]=1;
-        int dr=-1,dc=-1;
-        for (int i=0;i<n;i++){
-            for (int j=0;j<m;j++){
-              if (obstacleGrid[i][j]==1){
-                continue;
-              }
-              if (i+dr>=0){
-                dp[i][j]+=dp[i+dr][j];
-              }
-              if (j+dc>=0){
-                  dp[i][j]+=dp[i][j+dc];
-              }
-
+           vector<int>prev(m,0);
+           for (int i=0;i<m;i++){
+            if (obstacleGrid[0][i]==1){
+                break;
             }
+            if (obstacleGrid[0][i]!=1){
+            prev[i]=1;
+            }
+           }
+       
+        for (int i=1;i<n;i++){
+            vector<int>temp(m,0);
+            for(int j=0;j<m;j++){
+                if (obstacleGrid[i][j]){
+                  continue;
+                }
+               temp[j]+=prev[j];
+               if (j!=0){
+                temp[j]+=temp[j-1];
+               }
+               
+            }
+            prev=temp;
         }
-        return dp[n-1][m-1];
+        return prev[m-1];
     }
 };
