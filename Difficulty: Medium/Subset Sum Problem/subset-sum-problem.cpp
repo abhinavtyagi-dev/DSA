728853bd509dd1e68cd0638a2e30f89d9c882aa9
Abhinav Tyagi <abhinavtyagi9068@gmail.com>
2026-08-13 22:10:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
@@ -1,40 +1,36 @@
 class Solution {
   public:
-    bool findsum(vector<int>& arr,int ind,int target, vector<vector<int>>&dp){
-        if (target==0){
-            return true;
-        }
-        if (ind==0){
-            if (arr[ind]==target){
-                return true;
-            }
-            else{
-                dp[ind][target]=0;
-                return false;
-            }
-        }
-            if (dp[ind][target]!=-1){
-            return dp[ind][target];
-        } 
-        
-        bool nottake=findsum(arr,ind-1,target,dp);
-        if (nottake){
-        return true;
-    }
-    bool take=false;
-    if (target>=arr[ind]){
-        take=findsum(arr,ind-1,target-arr[ind],dp);
-    }
-    if (take){
-        return true;
-    }
-    dp [ind][target]=(nottake||take);
-    return (nottake||take);
-        }
     bool isSubsetSum(vector<int>& arr, int sum) {
         int n=arr.size();
-        vector<vector<int>>dp(n,vector<int>(sum+1,-1));
-        bool ans=findsum(arr,n-1,sum,dp);
-        return ans;
+     vector<vector<int>> dp(n,vector<int>(sum+1,-1));
+     dp[0][sum]=sum;
+     for (int i=1;i<n;i++){
+         for (int j=0;j<=sum;j++){
+             if (dp[i-1][j]==0){
+                 return true;
+             }
+             if (dp[i-1][j]!=-1){
+                 int val=dp[i-1][j];
+                 dp[i][val]=val;
+                 
+                 int left=val-arr[i-1];
+                 if (left>=0){
+                 dp[i][left]=left;
+                 }
+             }
+         }
+     }
+     for (int i=0;i<=sum;i++){
+         if (dp[n-1][i]==0){
+             return true;
+         }
+         if (dp[n-1][i]!=-1){
+             if (dp[n-1][i]==arr[n-1]){
+                 return true;
+             }
+         }
+     }
+     return false;
+        
     }
 };
