dcf99b43a81a615ef7c97643b95378d387f3f68b
Abhinav Tyagi <abhinavtyagi9068@gmail.com>
2026-08-16 00:25:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -0,0 +1,35 @@
+class Solution {
+  public:
+  int findsum(vector<int>& val,vector<int>& wt,int k,int ind,vector<vector<int>>& dp){
+    if (k<=0){
+        return 0;
+    }
+    if (ind==0){
+        if (wt[0]<=k){
+            return val[0];
+        }
+        else{
+            return 0;
+        }
+    }
+    if (dp[ind][k]!=-1){
+        return dp[ind][k];
+    }
+
+    int nottake=findsum(val,wt,k,ind-1,dp);
+    int take=0;
+    if (wt[ind]<=k){
+        take=val[ind]+findsum(val,wt,k-wt[ind],ind-1,dp);
+    }
+    dp[ind][k]=max(nottake,take);
+    return max(nottake,take);
+  }
+
+    int knapsack(int W, vector<int> &val, vector<int> &wt) {
+       
+        int n=val.size();
+         vector<vector<int>>dp(n,vector<int>(W+1,-1));
+       int ans=findsum(val,wt,W,n-1,dp);
+       return ans;
+    }
+};
