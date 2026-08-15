372324b3bd3afc24d9bf872c28fb21fc0f59be5c
Abhinav Tyagi <abhinavtyagi9068@gmail.com>
2026-08-15 12:15:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Subsets with Sum/count-subsets-with-sum.cpp
+++ Difficulty: Medium/Count Subsets with Sum/count-subsets-with-sum.cpp
@@ -1,37 +1,28 @@
 class Solution {
   public:
-    int findsum(vector<int>& arr, vector<vector<int>>& dp,int ind,int k){
-      if (ind==0&&k==0&&arr[0]==0){
-          return 2;
-      }
-      else if (ind==0&&k==0){
-          return 1;
-      }
-        else if (ind==0){
-            if (arr[0]==k){
-                return 1;
-        }
-        else{
-            return 0;
-        }
-        
-    }
-    if (dp[ind][k]!=-1){
-        return dp[ind][k];
-    }
-    int nottake=findsum(arr,dp,ind-1,k);
-    int take=0;
-    if (arr[ind]<=k){
-        take=findsum(arr,dp,ind-1,k-arr[ind]);
-    }
-    dp[ind][k]=take+nottake;
-    return take+nottake;
-    }
     int perfectSum(vector<int>& arr, int target) {
-        int n =arr.size();
-      vector<vector<int>> dp(n,vector<int> (target+1,-1));
-      int ans=findsum(arr,dp,n-1,target);
-      return ans;
-        
+        int n=arr.size();
+        vector<vector<int>> dp(n,vector<int>(target+1,0));
+        dp[0][0]=1;
+        if (arr[0]==0){
+            dp[0][0]=2;
+        }
+        else {
+            if (arr[0]<=target){
+            dp[0][arr[0]]=1;
+            }
+        }
+        for (int i=1;i<n;i++){
+            
+            for (int j=0;j<=target;j++){
+                int nottake=dp[i-1][j];
+                int take=0;
+                if (arr[i]<=j){
+                    take=dp[i-1][j-arr[i]];
+                }
+                dp[i][j]=take+nottake;
+            }
+        }
+        return dp[n-1][target];
     }
 };
