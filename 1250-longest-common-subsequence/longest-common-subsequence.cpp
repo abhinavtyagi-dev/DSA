2f540a6839632567d8d650d1c08586efb1208fc7
Abhinav Tyagi <abhinavtyagi9068@gmail.com>
2026-08-18 18:40:54 +0530

    Time: 23 ms (81.84%) | Memory: 26.5 MB (81.94%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -2,23 +2,25 @@ class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
         int n=text1.size(),m=text2.size();
-        vector<vector<int>> dp(n,vector<int>(m,0));
+        vector<int>dp(m,0);
         for (int i=0;i<m;i++){
-            if (text1[0]==text2[i]){
-                dp[0][i]=1;
+            if (text2[i]==text1[0]){
+                dp[i]=1;
             }
             else{
-                if (i!=0){
-                dp[0][i]=dp[0][i-1];
+                if(i!=0){
+                    dp[i]=dp[i-1];
                 }
             }
         }
+        
         for (int i=1;i<n;i++){
+            vector<int>curr(m,0);
             for (int j=0;j<m;j++){
                 int match=0,notmatch=0;
                 if (text1[i]==text2[j]){
                 if (j!=0){
-                    match=1+dp[i-1][j-1];
+                    match=1+dp[j-1];
                 }
                 else{
                     match=1;
@@ -26,15 +28,16 @@ public:
             }
             else{
                   int notmatch1=0,notmatch2=0;
-                 notmatch1=dp[i-1][j];
+                 notmatch1=dp[j];
                 if (j!=0){
-                    notmatch2=dp[i][j-1];
+                    notmatch2=curr[j-1];
                 }
                 notmatch=max(notmatch1,notmatch2);
             }
-            dp[i][j]=notmatch+match;
+            curr[j]=notmatch+match;
             }
+            dp=curr;
         }
-        return dp[n-1][m-1];
+        return dp[m-1];
     }
 };
