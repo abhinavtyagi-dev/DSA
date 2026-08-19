67cc7ed3dc09ee1516b1ea6117f55855dca69926
Abhinav Tyagi <abhinavtyagi9068@gmail.com>
2026-08-20 03:04:50 +0530

    Time: 95 ms (19.21%) | Memory: 76.1 MB (8.18%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
-int longestpallin(string& s,int ind1,int ind2,vector<vector<int>>& dp){
-       if (ind1>ind2){
-           return 0;
-       }
-       if (ind1==ind2){
-           return 1;
-       }
-       if (dp[ind1][ind2]!=-1){
-        return dp[ind1][ind2];
-       }
-       int match=0,notmatch=0;
-       if (s[ind1]==s[ind2]){
-           match=2+longestpallin(s,ind1+1,ind2-1,dp);
-       }
-       else{
-           int notmatch1=longestpallin( s,ind1+1,ind2,dp);
-             int notmatch2=longestpallin( s,ind1,ind2-1,dp);
-           notmatch=max(notmatch1,notmatch2);
-       }
-       dp[ind1][ind2]=match+notmatch;
-       return (match+notmatch);
-   }
     int longestPalindromeSubseq(string s) {
-          int n=0,m=s.length();
-          vector<vector<int>> dp(m,vector<int>(m,-1));
-       int ans=longestpallin(s,n,m-1,dp);
-       return ans;
+        int n=s.length();
+string t = s;
+reverse(t.begin(), t.end());
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        for (int i=0;i<n;i++){
+            for (int j=0;j<n;j++){
+                int match=0,notmatch=0;
+                if (s[i]==t[j]){
+                    match=1+dp[i][j];
+                }
+                else{
+                    int notmatch1=dp[i+1][j];
+                    int notmatch2=dp[i][j+1];
+                    notmatch=max(notmatch1,notmatch2);
+                }
+                dp[i+1][j+1]=match+notmatch;
+            }
+        }
+        return dp[n][n];
     }
 };
