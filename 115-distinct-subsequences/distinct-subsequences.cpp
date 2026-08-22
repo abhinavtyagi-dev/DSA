8f8603da8df2eb65b71066172de91e76f81d9152
Abhinav Tyagi <abhinavtyagi9068@gmail.com>
2026-08-22 23:46:44 +0530

    Time: 38 ms (23.97%) | Memory: 44.2 MB (39.61%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
-    int findt(string& s,string& t,int ind1,int ind2,vector<vector<int>>& dp){
-        if (ind2<0){
-            return 1;
-        }
-        if (ind1<0){
-            return 0;
-        }
-
-        if (dp[ind1][ind2]!=-1){
-            return dp[ind1][ind2];
-        }
-          int match1=0,match2=0;
-          int notmatch=0;
-          if (s[ind1]==t[ind2]){
-              match1+=findt(s,t,ind1-1,ind2-1,dp);
-              match2+=findt(s,t,ind1-1,ind2,dp);
-          }
-          else{
-             notmatch+=findt(s,t,ind1-1,ind2,dp);
-          }
-          dp[ind1][ind2]=match1+match2+notmatch;
-          return dp[ind1][ind2];
-    }
     int numDistinct(string s, string t) {
         int n=s.size(),m=t.size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        int ans=findt(s,t,n-1,m-1,dp);
-        return ans;
+        vector<vector<long long>> dp(n+1,vector<long long>(m+1,0));
+        int mod=1e9+7;
+        for (int i=0;i<n;i++){
+            dp[i][0]=1;
+
+        }
+        for (int i=0;i<n;i++){
+            for (int j=0;j<m;j++){
+                long long match1=0,match2=0;
+                long long notmatch=0;
+                if (s[i]==t[j]){
+                    match1+=(dp[i][j])%mod;
+                    match2+=(dp[i][j+1])%mod;
+                }
+                else{
+                    notmatch+=(dp[i][j+1])%mod;
+                }
+                dp[i+1][j+1]=(match1+match2+notmatch)%mod;
+            }
+        }
+        return (int)dp[n][m];
     }
 };
