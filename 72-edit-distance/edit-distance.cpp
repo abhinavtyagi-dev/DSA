9f09521775444f7837c5e9d15d671f7a06939677
Abhinav Tyagi <abhinavtyagi9068@gmail.com>
2026-08-23 01:50:19 +0530

    Time: 4 ms (81.36%) | Memory: 13.1 MB (73.96%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,35 +1,26 @@
 class Solution {
 public:
-    int convert(string& s1,string& s2,int ind1,int ind2,vector<vector<int>>& dp){
-        if (ind1<0&&ind2<0){
-            return 0;
-        }
-        else if (ind1<0){
-            return (ind2+1);
-        }
-        else if(ind2<0){
-            return (ind1+1);
-        }
-        if (dp[ind1][ind2]!=-1){
-            return dp[ind1][ind2];
-        }
-        
-        if (s1[ind1]!=s2[ind2]){
-           int notmatch1 =1+convert(s1,s2,ind1-1,ind2-1,dp);
-           int notmatch2=1+convert(s1,s2,ind1-1,ind2,dp);
-           int notmatch3=1+convert(s1,s2,ind1,ind2-1,dp);
-           dp[ind1][ind2]=min(notmatch1,notmatch2);
-           dp[ind1][ind2]=min(notmatch3,dp[ind1][ind2]);
-        }
-        else{
-            dp[ind1][ind2]=convert(s1,s2,ind1-1,ind2-1,dp);
-        }
-        return dp[ind1][ind2];
-    }
     int minDistance(string word1, string word2) {
         int n=word1.size(),m=word2.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        int ans=convert(word1,word2,n-1,m-1,dp);
-        return ans;
+        vector<vector<int>> dp (n+1,vector<int>(m+1,0));
+        for (int i=0;i<m;i++){
+            dp[0][i+1]=i+1;
+        }
+        for (int i=0;i<n;i++){
+            dp[i+1][0]=i+1;
+        }
+        for (int i=1;i<=n;i++){
+            for (int j=1;j<=m;j++){
+                if (word1[i-1]!=word2[j-1]){
+                    dp[i][j]=1+min(dp[i-1][j-1],dp[i-1][j]);
+                    dp[i][j]=min(dp[i][j],1+dp[i][j-1]);
+
+                }
+                else{
+                    dp[i][j]=dp[i-1][j-1];
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
