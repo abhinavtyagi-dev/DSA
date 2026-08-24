542b2dc921b346cce2f1efe9e70ccafbef9fcc35
Abhinav Tyagi <abhinavtyagi9068@gmail.com>
2026-08-25 00:11:42 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (74.43%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        vector<vector<int>> dp(n+2,vector<int>(2,0));
+        int n=prices.size();// isme yrr do row chiye hongi hme last wali do
+        vector<vector<int>> prev(2,vector<int>(2,0));
+
         for (int i=n-1;i>=0;i--){
+            vector<int> curr(2,0);
             for (int j=0;j<=1;j++){
                 int profit=0;
                 if (j){
-                    profit=max(-prices[i]+dp[i+1][0],dp[i+1][1]);
+                    profit=max(-prices[i]+prev[0][0],prev[0][1]);
                 }
                 else{
-                    profit=max(prices[i]+dp[i+2][1],dp[i+1][0]);
+                    profit=max(prices[i]+prev[1][1],prev[0][0]);
                 }
-                dp[i][j]=profit;
+                curr[j]=profit;
+
+            }
+            for (int i=0;i<=1;i++){
+                prev[1][i]=prev[0][i];
+                prev[0][i]=curr[i];
             }
             
         }
-        return dp[0][1];
+        return prev[0][1];
     }
 };
