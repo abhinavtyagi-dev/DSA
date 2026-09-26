82f4dd228824aabd2320a5234a40443fba743e91
Abhinav Tyagi <abhinavtyagi9068@gmail.com>
2026-09-26 17:19:12 +0530

    Time: 159 ms (14.64%) | Memory: 371.2 MB (6.23%) - LeetSync

--- 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.cpp
+++ 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string evaluate(string& s, vector<vector<string>>& knowledge) {
         string ans="";
-        map<string,string> mpp;
+        unordered_map<string,string> mpp;
         for (int i=0;i<knowledge.size();i++){
             mpp[knowledge[i][0]]=knowledge[i][1];
         }
@@ -16,7 +16,7 @@ public:
             }
           
             if (mpp.find(s1)!=mpp.end()){
-            ans+=mpp[s1];
+            ans+=mpp[s1];// ans=ans+mpp[s1] me copy create hoti h to bht jyada copies create ho jati for big input
             }
             else{
                ans=ans+"?";
